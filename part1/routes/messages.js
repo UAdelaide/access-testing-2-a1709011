ed9f0ae663a1a9feaa1195fd823450f0ffbd0c53
KarenWong <a1709011@student.adelaide.edu.au>
2025-06-18 15:43:27 +0000

    [automatic commit] 2025-06-18 15:43:27 UTC 27.33.121.122

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
+var CURRENT_BUYER_ID = 1;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
